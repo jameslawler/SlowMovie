a5a39deb157ae3e1d48e1d942cade605f58a3b27
jameslawler <services@jameslawler.com>
2020-12-20 21:57:33 +0100

    Default delay 300 seconds

--- argconfig.py
+++ argconfig.py
@@ -2,7 +2,7 @@ import argparse
 
 def getArgs():
   parser = argparse.ArgumentParser(description='SlowMovie Settings')
-  parser.add_argument('-d', '--delay', default=120,
+  parser.add_argument('-d', '--delay', default=300,
     help="Delay between screen updates, in seconds")
   parser.add_argument('-s', '--start',
     help="Start at a specific frame (default is read from file)")
